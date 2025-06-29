5af9afb03ba844bce8e2457fffca88ac448809fb
eunwol1991 <eunwol1991@gmail.com>
2025-06-29 15:35:32 +0800

    pepper lunch pricing udpate by renamemos.py

--- copypasterfile/renamemos.py
+++ copypasterfile/renamemos.py
@@ -35,22 +35,22 @@ def update_excel_files(directory, debug=False):
                                 for cell in row:
                                     cell_value = str(cell.value).strip().lower() if cell.value is not None else ""
                                     # 模糊匹配 "tartar sauce"
-                                    cell_name = "efgh"
+                                    cell_name = "savori instant mashed potato"
                                     
                                     if cell_name in cell_value:
                                         row_number = cell.row
-                                        product_code = "AUFG801EK"
-                                        product_description = "Kranch Alaska Pollock Finger"
-                                        pack_size = "(5 x 900g)"
-                                        qty = 1.00
-                                        UOM = "CTN"
-                                        selling_price = f"=11.59*G{row_number}*5"
+                                        product_code = "13294632"
+                                        product_description = "Savori Instant Mashed Potato"
+                                        pack_size = "(5 x 2KG)"
+                                        qty = 2.00
+                                        UOM = "CTNS"
+                                        selling_price = f"=5.9*2*G{row_number}*5"
                                         if debug:
                                             print(f"🔍 匹配到 {cell_name} → Sheet: {sheet_name}, Row: {row_number}")
                                         if sheet_name == "DO":
                                             sheet[f"B{row_number}"].value = product_code
                                             sheet[f"C{row_number}"].value = product_description
-                                            sheet[f"H{row_number}"].value = pack_size
+                                            sheet[f"G{row_number}"].value = pack_size
                                             sheet[f"I{row_number}"].value = qty
                                             sheet[f"K{row_number}"].value = UOM
                                             if debug:
@@ -88,5 +88,5 @@ def update_excel_files(directory, debug=False):
             print(f"  ❌ {error_file}")
 
 # 示例调用
-directory_path = r"C:\Users\User\Dropbox\DO & INV\DO & INV 2025\Melvin - MOS Burger\For Customer\MOS DOC (OTL) - Format"
+directory_path = r"C:\Users\User\Dropbox\DO & INV\DO & INV 2025\Melvin - Pepperlunch\Format"
 update_excel_files(directory_path, debug=True)
